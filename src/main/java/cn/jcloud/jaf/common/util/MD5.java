2cae7bc408b5e055d51fbce4c9e1e09edc80fa8d
位函 <weihan.615@163.com>
2018-03-29 08:43:34 +0800

    完善编码规范

--- src/main/java/cn/jcloud/jaf/common/util/MD5.java
+++ src/main/java/cn/jcloud/jaf/common/util/MD5.java
@@ -38,7 +38,7 @@ public class MD5 {
 				// 创建具有指定算法名称的信息摘要
 				MessageDigest md = MessageDigest.getInstance("MD5");
 				// 使用指定的字节数组对摘要进行最后更新，然后完成摘要计算
-				byte[] results = md.digest(originString.getBytes());
+				byte[] results = md.digest(originString.getBytes("UTF-8"));
 				// 将得到的字节数组变成字符串返回
 				String resultString = byteArrayToHexString(results);
 				return resultString.toUpperCase();
